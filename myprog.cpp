3f24ad22054fc860de0ea39aa939ec02481fdb94
KozhVsevolod <71258927+KozhVsevolod@users.noreply.github.com>
2021-06-10 16:03:24 +0300

    Add files via upload

---
+++ myprog.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{                                                                                                                                           
+    int row1, row2, col1, col2;
+    double** a, ** b, ** c;
+                                                                                                                                            
+    row1 = 500;
+    col1 = 500;
+    row2 = 500;
+    col2 = 500;
+                                                                                                                                            
+    a = new double* [row1];
+    for (int i = 0; i < row1; i++)
+    {                                                                                                                                       
+        a[i] = new double[col1];
+        for (int j = 0; j < col1; j++)
+        {                                                                                                                                   
+            a[i][j] = i + j;                                                                                                                
+        }                                                                                                                                   
+    }                                                                                                                                       
+                                                                                                                                            
+    b = new double* [row2];
+    for (int i = 0; i < row2; i++)
+    {                                                                                                                                       
+        b[i] = new double[col2];
+        for (int j = 0; j < col2; j++)
+        {                                                                                                                                   
+            b[i][j] = i + j;                                                                                                                
+        }                                                                                                                                   
+    }                                                                                                                                       
+    for (int k = 0; k < 120000; ++k)
+    {                                                                                                                                       
+    c = new double* [row1];
+    for (int i = 0; i < row1; i++)
+    {                                                                                                                                       
+        c[i] = new double[col2];
+        for (int j = 0; j < col2; j++)
+        {                                                                                                                                   
+            c[i][j] = 0;
+            for (int k = 0; k < col1; k++)
+                c[i][j] += a[i][k] * b[k][j];                                                                                               
+        }                                                                                                                                   
+    }                                                                                                                                       
+    cin.get(); cin.get();                                                                                                                   
+    }                                                                                                                                       
+    return 0;
+}
---
+++ myscript.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+path='/home/grid/testbed/tb041/Yavkin/lab3/'
+cd $path
+                                                                                                                                            
+flagsForCmplrOptmztion=(O0 O1 O2 O3 Os Ofast)
+i=0
+                                                                                                                                            
+for flag in ${flagsForCmplrOptmztion[@]}; do
+        srcfile="result$i"
+        g++ -$flag myprog.cpp -o $srcfile
+        time ./$srcfile
+        let "i=i+1"
+        echo -e "\n"
+done
+                                                                                                                                            
+ml icc                                                                                                                                      
+flagsForIccCpu=$(cat /proc/cpuinfo | grep flags | uniq | cut -d: -f2 | cut -d" " -f2- | tr _ .)
+flagsForIccOptmztion=(O2 Ofast)
+                                                                                                                                            
+for optmzFlag in ${flagsForIccOptmztion[@]}; do
+        for iccFlag in $flagsForIccCpu; do
+                srcfile="iccResult$iccFlag$optmzFlag"
+                icc -$optmzFlag -qopt-report-phase=vec myprog.cpp -o $srcfile -x$iccFlag 2> errors.txt
+                if [ $? -eq 0 ]
+                then
+                        echo "icc compilation with -$optmzFlag flag and $iccFlag cpu extension:"
+                        time ./$srcfile
+                        echo -e "\n"
+                fi
+        done
+done
